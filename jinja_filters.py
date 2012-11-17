48a96dbb4f35c0c04390e8b156b6cd39eab4b17a
Phillip Quiza <aznwhtey@gmail.com>
2012-11-17 14:36:03 -0500

    Added a filter to format youtube insights data

--- jinja_filters.py
+++ jinja_filters.py
@@ -7,3 +7,11 @@ def humanize(name):
     Returns the human readable form of a miner 
     """
     return miners.get_name(name) 
+
+def format_insights(data):
+    """
+    Takes raw youtube insights data and returns a dict of the parsed insight
+    data
+    """
+    miner = miners._load_miner('youtube_insights')
+    return miner.parse(data)
